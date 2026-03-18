c47fa9554569467ef9f8ffb478689134e5e690c4
KingRJ3 <rilesronn@gmail.com>
2026-03-18 08:05:03 -0500

    Commits before making branch

--- zombie-bot-game-jam/Scripts/player_body.gd
+++ zombie-bot-game-jam/Scripts/player_body.gd
@@ -45,7 +45,9 @@ func _physics_process(delta: float) -> void:
 
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("left-click"):
+	claws_hitbox.disabled = true
+	if Input.is_action_pressed("left-click"):
+		claws_hitbox.disabled = false
 		if override_direction_input == "right":
 			claws_hitbox.rotation = 0
 			claws_hitbox.position = Vector2(23,0)
